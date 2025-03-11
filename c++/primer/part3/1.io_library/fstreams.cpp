f127f67f3d012679f540c227289f83d8a385e3c5
ird <irdali1996@gmail.com>
2025-03-11 18:30:19 +0900

    added check for if file exists before copying

--- c++/primer/part3/1.io_library/fstreams.cpp
+++ c++/primer/part3/1.io_library/fstreams.cpp
@@ -9,15 +9,23 @@ using std::string;
 void excercise1(const string &file1, const string &file2){
     /*
     code to copy contents of a file from another file
+    reads the text from file and writes to file2 (paths)
     */
    
     std::ifstream input(file1, std::ios::in);
+    if(!input)
+        std::cerr << "unable to open the readfile, make sure it exists" << std::endl;
+    
     std::ofstream output(file2, std::ios::out);
+    if(!output)
+        std::cerr << "unable to open the write file, make sure it exists" << std::endl;
 
     string s;
     while(std::getline(input,s))
         output << s << std::endl;
 
+    input.close();
+    output.close()
 }
 
 
